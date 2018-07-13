90081f8fc261a9e65bdee7d8c2b8c967c0dd9e11
nathanhleung <nathanhleung@users.noreply.github.com>
2018-07-12 23:51:00 -0400

    drop day

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const makeCountdown = require('countdown');
+const leftPad = require('left-pad');
+
+// Midnight on 7/13/2018
+const DROP_DATE = 1531454400000;
+
+function tick() {
+  const { days, hours, minutes, seconds, value } = makeCountdown(DROP_DATE);
+  const countdownEl = document.getElementById('countdown');
+  countdownEl.innerHTML =
+    `${leftPad(minutes, 2, '0')}:${leftPad(seconds, 2, '0')}`;
+
+  // Value will be positive if time is past
+  if (value > 0) {
+    window.location.replace('/drop');
+  }
+}
+
+tick();
+
+setInterval(tick, 1000);
